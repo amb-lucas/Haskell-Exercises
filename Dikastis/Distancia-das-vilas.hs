e02ede8e8feb074cfb45b8fbaf748fe31dca6ac0
Lucas Ambrósio <38929679+amb-lucas@users.noreply.github.com>
2021-02-10 23:04:33 -0300

    01EH1GTCEB4HGXRPZ0AEHBCPDA incomplete (WA)

---
+++ Dikastis/Distancia-das-vilas.hs
@@ -0,0 +1,28 @@
+truncate' :: Double -> Int -> Double
+truncate' x n = truncate'' (x + (5/t)) n
+  where t = 10^(n+1)
+
+truncate'' :: Double -> Int -> Double
+truncate'' x n = (fromIntegral (floor (x * t))) / t
+  where t = 10^n
+    
+dist :: Double -> Double -> Double -> Double -> Double
+dist x1 y1 x2 y2 = sqrt (dx*dx + dy*dy)
+  where dx = x1 - x2
+        dy = y1 - y2
+
+solve :: Double -> Double -> String
+solve x y = hogsmeadeLine ++ kakarikoLine ++ solitudeLine
+  where hogsmeadeLine = "Distancia para Hogsmeade: " ++
+                        (show $ truncate' (dist x y 34 220) 2) ++ "\n"
+        kakarikoLine = "Distancia para Kakariko: " ++
+                        (show $ truncate' (dist x y 0 0) 2) ++ "\n"
+        solitudeLine = "Distancia para Solitude: " ++
+                        (show $ truncate' (dist x y 140 456) 2)
+
+main = do {
+  x <- readLn;
+  y <- readLn;
+  
+  putStr $ solve x y
+}
