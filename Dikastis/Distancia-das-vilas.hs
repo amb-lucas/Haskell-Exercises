fc0fd146c1d98a61f8ec6d3b37abc05f6b959b58
Lucas Ambrósio <38929679+amb-lucas@users.noreply.github.com>
2021-02-11 00:27:45 -0300

    01EH1GTCEB4HGXRPZ0AEHBCPDA solved

--- Dikastis/Distancia-das-vilas.hs
+++ Dikastis/Distancia-das-vilas.hs
@@ -1,3 +1,15 @@
+truncateFormat :: Double -> Int -> String
+truncateFormat num 0 = show (truncate' num 0)
+truncateFormat num n = truncateFormat' (show (truncate' num n)) n False
+
+truncateFormat' :: String -> Int -> Bool -> String
+truncateFormat' _ 0 True          = ""
+truncateFormat' "" n True         = "0" ++ (truncateFormat' "" (n-1) True)
+truncateFormat' (a:as) n True     = a:(truncateFormat' as (n-1) True)
+truncateFormat' "" n False        = "." ++ (truncateFormat' "" n True)
+truncateFormat' ('.':as) n False  = "." ++ (truncateFormat' as n True)
+truncateFormat' (a:as) n False    = a:(truncateFormat' as n False)
+
 truncate' :: Double -> Int -> Double
 truncate' x n = truncate'' (x + (5/t)) n
   where t = 10^(n+1)
@@ -14,11 +26,11 @@ dist x1 y1 x2 y2 = sqrt (dx*dx + dy*dy)
 solve :: Double -> Double -> String
 solve x y = hogsmeadeLine ++ kakarikoLine ++ solitudeLine
   where hogsmeadeLine = "Distancia para Hogsmeade: " ++
-                        (show $ truncate' (dist x y 34 220) 2) ++ "\n"
+                        (truncateFormat (dist x y 34 220) 2) ++ "\n"
         kakarikoLine = "Distancia para Kakariko: " ++
-                        (show $ truncate' (dist x y 0 0) 2) ++ "\n"
+                        (truncateFormat (dist x y 0 0) 2) ++ "\n"
         solitudeLine = "Distancia para Solitude: " ++
-                        (show $ truncate' (dist x y 140 456) 2)
+                        (truncateFormat (dist x y 140 456) 2)
 
 main = do {
   x <- readLn;
